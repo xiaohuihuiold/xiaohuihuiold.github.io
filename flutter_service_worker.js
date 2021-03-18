111b737869d22ae52d3b2ececf0672a652c3cc24
xiaohuihuiold <xiaohuihuiold@gmail.com>
2021-03-19 01:57:23 +0800

    Add animation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a7ba56a57c89b38a096edb668751e086",
-"/": "a7ba56a57c89b38a096edb668751e086",
-"main.dart.js": "78dea11ee002ee383b373f30782ca160",
+"index.html": "aacab09be752173d4d87a0620ee7af44",
+"/": "aacab09be752173d4d87a0620ee7af44",
+"main.dart.js": "7d0fc062c017978cc51e359db353318c",
 "manifest.json": "ba2ca419d7be524a520276192af4d9ba",
 "version.json": "f7f4b2e68215a1b05de45dfc65727dcf"
 };
